ba79aeb9693b8abd174c98e63f049b9724eed1db
Leticia-Lopes2 <leticiamouralopes5@gmail.com>
2024-03-11 19:57:36 -0300

    refactor: Criando componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
+import { useState, useEffect, useCallback } from 'react';
 
-import './App.css'
-import StartScreen from './components/StartScreen'
+import StartScreen from './components/StartScreen';
+
+import './App.css';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+import { Words } from './data/Words';
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 1, name: "game" },
+  { id: 1, name: "end" }
+]
 
 export default function App() {
 
+  const [ gameStage, setGameStage ] = useState(stages[0].name);
+  
+  const [ wordsList ] = useState(Words);
+  console.log(wordsList);
+
   return (
+    
     <div className="flex justify-center items-center text-center text-white">
-      <StartScreen />
+      {gameStage === 'start' && <StartScreen />}
+      {gameStage === 'game' && <Game />}
+      {gameStage === 'end' && <GameOver />}
     </div>
+
   )
 }
